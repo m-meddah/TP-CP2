083c3d5a844702dd40ac4f0cc634193bf4b6016d
m-meddah <meddah.madjid@gmail.com>
2022-09-04 16:01:50 +0200

    new challenge take for example

--- app/database.js
+++
@@ -1,7 +0,0 @@
-const pg = require('pg');
-
-const client = new pg.Client(process.env.PG_URL);
-
-client.connect();
-
-module.exports = client;
--- app/middlewares/initLocals.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-    res.locals.pageDescription = 'Site de ventes de figurines de Final Fantasy VII';
-    res.locals.pageTitle = `O'Fig - un magasin de figurines`;
-    next();
-}
